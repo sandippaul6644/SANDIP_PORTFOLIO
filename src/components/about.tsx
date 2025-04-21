8726616495742b95147b7fc9ef2b9f9974fa1e5d
SANDIP PAUL <sandippaul7318@gmail.com>
2025-04-22 00:22:38 +0530

    first commit

---
+++ src/components/about.tsx
@@ -0,0 +1,76 @@
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { SERVICES } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
+type ServiceCardProps = {
+  index: number;
+  title: string;
+  icon: string;
+};
+
+// Service Card
+const ServiceCard = ({ index, title, icon }: ServiceCardProps) => {
+  return (
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="xs:w-[250px] w-full"
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+// About
+export const About = () => {
+  return (
+    <SectionWrapper idName="about">
+      <>
+        {/* Title */}
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </motion.div>
+
+        {/* Body */}
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="empty-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          As a dynamic and motivated Software Engineer, 
+          I am committed to learning and mastering cutting-edge technologies.
+           As an graduate software developer, I have cultivated a strong foundation in web development,
+            specializing in the MERN stack, Next.js, and Firebase. Additionally, 
+            I am proficient in working with RESTful APIs, Docker, and Git. 
+            My experience includes building and deploying scalable applications and integrating AI capabilities through Genkit AI.
+             Driven by a passion for continuous learning and problem-solving, 
+             I am eager to contribute to innovative projects and collaborate in dynamic tech environments.
+        </motion.p>
+
+        {/* Service Card */}
+        <div className="mt-20 flex flex-wrap gap-10">
+          {SERVICES.map((service, i) => (
+            <ServiceCard key={service.title} index={i} {...service} />
+          ))}
+        </div>
+      </>
+    </SectionWrapper>
+  );
+};
